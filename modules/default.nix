71bad346675663e0365c343ba805a4d2628a1828
Johan Boule <johan.boule@retropaganda.info>
2019-03-31 03:55:09 +0200

    cosmetics

--- modules/default.nix
+++ modules/default.nix
@@ -1,13 +1,13 @@
 with import <nixpkgs> {};
 derivation {
-	name = "bohan-module";
+	name = "cxx-lang-experiments-modules";
 	src = ./.;
 	inherit gnumake coreutils findutils gnused clang patchelf;
 	binutils = binutils-unwrapped;
 	builder = "${dash}/bin/dash";
 	args = [ "-c" ''
 		set -eux &&
-		export PATH="$gnumake/bin:$coreutils/bin:$findutils/bin:$clang/bin:$patchelf/bin:$binutils/bin"
+		export PATH="$gnumake/bin:$coreutils/bin:$findutils/bin:$gnused/bin:$clang/bin:$patchelf/bin:$binutils/bin"
 		make -f $src/Makefile -j$NIX_BUILD_CORES -O
 		mv main $out
 		patchelf --shrink-rpath $out
