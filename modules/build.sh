9a6cbcc4c2ef45a7050f3ed3db003fdbe3c0529a
Johan Boule <johan.boule@retropaganda.info>
2019-04-04 21:42:04 +0200

    drop any poor attempt at detecting gnu tools on non gnu system as i can't even test it. other cosmetic changes in the makefile

--- modules/build.sh
+++ modules/build.sh
@@ -4,26 +4,11 @@ set -eux &&
 
 cd "$(dirname "$0")"
 
-if test -n "${IN_NIX_SHELL:-}"
-then
-	clang=${CXX:-$clang/bin/c++}
-	gnumake=$gnumake/bin/make
-	gnused=$gnused/bin/sed
-	jobs=$NIX_BUILD_CORES
-else
-	clang=$(command -v ${CXX:-clang++})
-	gnumake=$(command -v gmake || command -v make)
-	gnused=$(command -v gsed || command -v sed)
-	jobs=$(getconf _NPROCESSORS_ONLN)
-fi
-
 bld_dir=++build
 
 mkdir -p $bld_dir
 
-exec $gnumake \
-	-C $bld_dir -f ../Makefile -j$jobs -O --no-print-directory \
-	CXX=$clang GNU_SED=$gnused \
+exec make \
+	-C $bld_dir -f ../Makefile \
+	-j$(getconf _NPROCESSORS_ONLN) -O --no-print-directory \
 	"$@"
-
-#	--warn-undefined-variables \
