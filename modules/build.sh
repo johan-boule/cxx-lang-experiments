ac62a4232a4f15b142772253202e0668812ae8f8
Johan Boule <johan.boule@retropaganda.info>
2019-04-29 21:09:58 +0200

    strace

--- modules/build.sh
+++ modules/build.sh
@@ -8,7 +8,8 @@ bld_dir=++wondermake-build
 
 mkdir -p $bld_dir
 
-exec ${MAKE:-make} \
-	-C $bld_dir -f ../GNUmakefile \
-	-j$(($(getconf _NPROCESSORS_ONLN) * 2)) -O --no-print-directory \
-	"$@"
+exec strace -cf \
+	${MAKE:-make} \
+		-C $bld_dir -f ../GNUmakefile \
+		-j$(($(getconf _NPROCESSORS_ONLN) * 2)) -O --no-print-directory \
+		"$@"
