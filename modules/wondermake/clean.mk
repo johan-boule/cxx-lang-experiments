49e9a33cc035a2edc9096da4cab1bb902a479e31
Johan Boule <johan.boule@retropaganda.info>
2019-05-05 06:22:59 +0200

    readded dependency from configure to toolchain env that was dropped by mistake. put back cat instead of file (for old compat), but mention the new way

--- modules/wondermake/clean.mk
+++ modules/wondermake/clean.mk
@@ -25,7 +25,7 @@ wondermake.clean:
 ifndef MAKE_RESTARTS # only do this on the first make phase
   wondermake.default: $(wondermake.bld_dir)wondermake.auto-clean
   $(wondermake.bld_dir)wondermake.auto-clean: wondermake.force | $(wondermake.bld_dir)
-	$(eval $@.old := $(subst $$,$$$$,$(shell cat $@ 2>/dev/null)))
+	$(eval $@.old := $(subst $$,$$$$,$(shell cat $@ 2>/dev/null))) # new way $(file < $@)
 	$(eval $@.new := $(sort $(wondermake.clean)))
 	$(if $(call wondermake.equals,$($@.old),$($@.new)), \
 		$(if $(wondermake.verbose),$(call wondermake.announce,auto-clean,,no change)) \
