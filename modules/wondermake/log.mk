7a19e179ab38d2517443aca8baa526b9967f057e
Johan Boule <johan.boule@retropaganda.info>
2019-06-18 22:55:54 +0200

    improved logging

--- modules/wondermake/log.mk
+++ modules/wondermake/log.mk
@@ -72,13 +72,13 @@ ifeq '' '$(if $(wondermake.verbose),,$(findstring s, $(firstword x$(MAKEFLAGS)))
     $(if $(MAKE_TERMOUT),,$(info ===============================================================================)) \
     $(if $4, \
       $(info \
-        $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style)$(wondermake.term.bold),{$1})) \
-        $(strip $(call wondermake.maybe_colored_out,$(wondermake.standout_style),$2)) \
+        $(strip $(call wondermake.maybe_colored_out,$(wondermake.standout_style)$(wondermake.term.bold),{$1})) \
+        $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style)$(wondermake.term.bold),$2)) \
         $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style),$3)) \
         $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style)$(wondermake.term.dim),$4)) \
       ), \
       $(info \
-        $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style)$(wondermake.term.bold),{$1})) \
+        $(strip $(call wondermake.maybe_colored_out,$(wondermake.standout_style)$(wondermake.term.bold),{$1})) \
         $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style),$2)) \
         $(strip $(call wondermake.maybe_colored_out,$(wondermake.info_style)$(wondermake.term.dim),$3)) \
       ) \
@@ -86,12 +86,12 @@ ifeq '' '$(if $(wondermake.verbose),,$(findstring s, $(firstword x$(MAKEFLAGS)))
   wondermake.announce_shell = \
     $(if $(MAKE_TERMOUT),,printf '===============================================================================\n';) \
     $(if $4, \
-      printf  '%s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style)$(wondermake.term.bold),{$1}); \
-      printf ' %s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.standout_style),$2); \
+      printf  '%s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.standout_style)$(wondermake.term.bold),{$1}); \
+      printf ' %s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style)$(wondermake.term.bold),$2); \
       printf ' %s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style),$3); \
       printf ' %s\n' $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style)$(wondermake.term.dim),$4) \
     , \
-      printf  '%s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style)$(wondermake.term.bold),{$1}); \
+      printf  '%s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.standout_style)$(wondermake.term.bold),{$1}); \
       printf ' %s'   $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style),$2); \
       printf ' %s\n' $(call wondermake.maybe_colored_out_shell,$(wondermake.info_style)$(wondermake.term.dim),$3) \
     )
