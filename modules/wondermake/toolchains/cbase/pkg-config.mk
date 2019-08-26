ce3a1edac575da76a8437ea2c7820437b20b3246
Johan Boule <johan.boule@retropaganda.info>
2019-08-26 04:14:11 +0200

    pkg-config: stop on fail

--- modules/wondermake/toolchains/cbase/pkg-config.mk
+++ modules/wondermake/toolchains/cbase/pkg-config.mk
@@ -34,12 +34,12 @@ define wondermake.cbase.pkg_config_command_cached_recurse # $1 = scope, $2 = arg
       $(wondermake.cbase.pkg_config_cache[$3].value), \
       $(call $0,$1,$2,$3x))
   ,
-    $(call wondermake.announce,pkg-config,$2) \
+    $(if $(wondermake.verbose),$(call wondermake.announce,pkg-config,$2)) \
     $(eval
       wondermake.cbase.pkg_config_cache[$3].key := $2
-      wondermake.cbase.pkg_config_cache[$3].value := $(shell $2)
+      wondermake.cbase.pkg_config_cache[$3].value := $(shell $2 || printf '%s ' '$$(call wondermake.error,'$2 'failed)')
     ) \
-    $(call wondermake.print,$(wondermake.cbase.pkg_config_cache[$3].value)) \
+    $(if $(wondermake.verbose),$(call wondermake.print,$(wondermake.cbase.pkg_config_cache[$3].value))) \
     $(if $(wondermake.verbose),$(call wondermake.announce,$1,$2,put new result into cache: $(wondermake.cbase.pkg_config_cache[$3].value))) \
     $(wondermake.cbase.pkg_config_cache[$3].value) \
   )
